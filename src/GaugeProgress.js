87e901cb8fc60a2c98cee189cac5bda2e6cc93ae
smlsunxie <smlsun@trunk-studio.com>
2021-01-08 16:37:15 +0800

    verson 1

--- src/GaugeProgress.js
+++ src/GaugeProgress.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View, Platform, ViewPropTypes, AppState } from 'react-native';
-import { Surface, Shape, Path, Group } from '@react-native-community/art';
+import { View, Platform, ViewPropTypes, AppState, Dimensions } from 'react-native';
+import { Surface, Shape, Path, Group, Text } from '@react-native-community/art';
 import MetricsPath from 'art/metrics/path';
-const ActiveState = "active"
+const ActiveState = "active";
+
+const { height, width } = Dimensions.get('window');
+const vw = width / 100;
+const vh = height / 100;
+
+const max = 50;
+const ticksGapfromProgressBar = 12;
 
 export default class GaugeProgress extends React.Component {
   constructor(props: Props) {
@@ -28,11 +35,18 @@ export default class GaugeProgress extends React.Component {
     return p;
   }
 
+  ticksPath(cx, cy, r, startDegree, endDegree) {
+    let p = Path();
+    p.path.push(0, cx + r, cy);
+    p.path.push(4, cx, cy, r, startDegree * Math.PI / 180, endDegree * Math.PI / 180, 1);
+    return p;
+  }
+
   extractFill(fill) {
     if (fill < 0.01) {
       return 0;
-    } else if (fill > 100) {
-      return 100;
+    } else if (fill > max) {
+      return max;
     }
 
     return fill;
@@ -41,19 +55,22 @@ export default class GaugeProgress extends React.Component {
   render() {
     const { size, width, tintColor, backgroundColor, style, stroke, strokeCap, rotation, cropDegree, children } = this.props;
     const backgroundPath = this.circlePath(size / 2, size / 2, size / 2 - width / 2, 0, (360 * 99.9 / 100) - cropDegree);
+    const ticksPath = this.ticksPath(size / 2, size / 2, size / 2 + ticksGapfromProgressBar, 0, (360 * 99.9 / 100) - cropDegree);
 
     const fill = this.extractFill(this.props.fill);
-    const circlePath = this.circlePath(size / 2, size / 2, size / 2 - width / 2, 0, ((360 * 99.9 / 100) - cropDegree) * fill / 100);
+    const circlePath = this.circlePath(size / 2, size / 2, size / 2 - width / 2, 0, ((360 * 99.9 / 100) - cropDegree) * fill / max);
     if (!this.state.isVisible) {
       return null;
     }
+
     return (
       <View style={style}>
         <Surface
-          width={size}
-          height={size}
+          width={size + 100}
+          height={(vh > 8.4) ? (size + 100) : (size + 70)}
           >
-          <Group rotation={rotation + cropDegree / 2} originX={size / 2} originY={size / 2}>
+          <Group rotation={rotation + cropDegree / 2} originX={(vw < 3.9) ? ((size + 10 * vw) / 2) : ((size + 8 * vw) / 2)} originY={(size + 8 * vh) / 2}>
+            <Shape d={ticksPath} stroke={backgroundColor} strokeWidth={2} />
             <Shape d={backgroundPath}
                    strokeDash={stroke}
                    stroke={backgroundColor}
